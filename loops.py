249de670e26d05626457f63bf5077f13521b1908
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2023-09-02 00:26:08 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  2 00:25:29 2023
+
+@author: shubh
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    if(n>=1 and n<=20):
+        for i in range(n):
+            print(i**2)
+        
+   
+    
