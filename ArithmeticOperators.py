9f0ab6bd5d9316fb7664bb5df89a3e3dca394bc8
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2023-09-02 00:24:05 +0530

    Add files via upload

---
+++ ArithmeticOperators.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  2 00:23:05 2023
+
+@author: shubh
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    if(a >=1 and a<=10**10  and b>=1 and b<=10**10):
+        print(a+b)
+        print(a-b)
+        print(a*b)
+    else:
+        pass
