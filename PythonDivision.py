72cb84dbd1808c14a263d2ae3a88f8bb0552c3f1
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2023-09-02 00:25:13 +0530

    Add files via upload

---
+++ PythonDivision.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  2 00:23:06 2023
+
+@author: shubh
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
