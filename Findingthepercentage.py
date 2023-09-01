f1f8afb9098b371e0fe8268f5e2c1da64cbebf70
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2023-09-02 00:32:57 +0530

    Add files via upload

---
+++ Findingthepercentage.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  2 00:32:17 2023
+
+@author: shubh
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    l1 = list(student_marks[query_name]) 
+
+    addition = sum(l1)
+
+    result = addition/len(l1)
+
+    print('%.2f'% result)
