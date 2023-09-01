0b6918940b482a303532e0271e13e83d4e2dfb34
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2023-09-02 00:28:55 +0530

    Add files via upload

---
+++ PrintFunction.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  2 00:27:47 2023
+
+@author: shubh
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    if(n>=1 and n<=150):
+        for i in range(1,n+1):
+            print(i,end='')
+    
+            
